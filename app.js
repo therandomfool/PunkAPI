1805946406fc4873cc55e20458c2a9071e3f8bb1
therandomfool <mddenver@protonmail.com>
2020-07-28 16:15:17 -0700

    put fetch in a function to be callen on button click

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://api.punkapi.com/v2/beers/random')
-  .then(response => {
-      return response.json()
-  })
-  .then(data => {
-      console.log(data)
-      const name = data[0].name
-      const description = data[0].description
-      const {volume} = data[0]
-      const volumeValue = volume.value 
-  })
+    const startBtn = document.querySelector('.beer-button')
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+
+    function getData() {
+
+        fetch('http://api.punkapi.com/v2/beers/random')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                console.log(data)
+                const name = data[0].name
+                const description = data[0].description
+                const {
+                    volume
+                } = data[0]
+                const volumeValue = volume.value
+                const volumeUnit = volume.unit
+
+                randomBeer.innerHTML = name + ' ' + volumeValue + volumeUnit
+                descriptionDisplay.innerHTML = description
+            })
+    }
+
+    startBtn.addEventListener('click', getData)
+
 })
