00c0ab51ea9c608455f947dcc6c3fa5dcc73cbca
therandomfool <mddenver@protonmail.com>
2020-07-28 15:52:07 -0700

    css basics html basics

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  fetch('http://api.punkapi.com/v2/beers/random')
+  .then(response => {
+      return response.json()
+  })
+  .then(data => {
+      console.log(data)
+      const name = data[0].name
+      const description = data[0].description
+      const {volume} = data[0]
+      const volumeValue = volume.value 
+  })
 })
